18cf90c781e718ddbd2535a5f5a292c180a426a7
ijcuevas2 <ijcuevas2@gmail.com>
2016-11-10 17:29:34 -0600

    some code

--- source_js/script.js
+++ source_js/script.js
@@ -1,3 +1,17 @@
 function searchBarController($scope){
     $scope.searchBar = {field:"Search For A Movie"};
 }
+
+$(".button.alert").click(function(){
+    var searchBarVal = $("#searchBar").val;
+    var theUrl       = "http://fa16-cs411-36.cs.illinois.edu:8080/";
+    $.post(theUrl,
+    {
+        queryVal: "searchBarVal"
+    });
+    // function(){
+    //     console.log("alrighty");
+    // });
+});
+
+
