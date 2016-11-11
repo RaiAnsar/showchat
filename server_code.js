aa798d3812ee8205d8ac0d308f9da342c1a11526
ijcuevas2 <ijcuevas2@gmail.com>
2016-11-10 18:13:09 -0600

    fixed styles

--- server_code.js
+++ server_code.js
@@ -2,8 +2,13 @@ var path    = require('path');
 var express = require('express');
 
 var app = express();
-var staticPath = path.join(__dirname, '/public');
-app.use(express.static(staticPath));
+app.use(express.static(__dirname + '/public'));
+app.use('/source_css', express.static(__dirname + '/source_css'));
+app.use('/source_js', express.static(__dirname + '/source_js'));
+app.use('/foundation', express.static(__dirname + '/foundation'));
+app.use('/foundation-icons', express.static(__dirname + '/foundation-icons'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
 
 const PORT = 8080;
 app.listen(PORT, function(){
