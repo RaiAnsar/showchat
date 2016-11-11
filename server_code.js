26fd6bb36902e2907226e297f680be2dd1cf1a67
ijcuevas2 <ijcuevas2@gmail.com>
2016-11-10 18:35:06 -0600

    new server code

--- server_code.js
+++ server_code.js
@@ -1,5 +1,11 @@
 var path    = require('path');
 var express = require('express');
+var http       = require('http');
+var fs         = require('fs');
+var formidable = require('formidable');
+var util       = require('util');
+var mysql      = require('mysql');
+var querystring = require('querystring');
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -9,8 +15,73 @@ app.use('/foundation', express.static(__dirname + '/foundation'));
 app.use('/foundation-icons', express.static(__dirname + '/foundation-icons'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
+app.set('port', process.env.PORT || 8080);
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'meeseeks',
+  database: 'ShowChat'
+});
 
-const PORT = 8080;
-app.listen(PORT, function(){
-    console.log('listening on port: ' + PORT);
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('You are now connected...');
+});
+
+
+app.get('/', function(req, res){
+ res.type('text/plain');
+ res.send('Showchat');
+});
+app.get('/{profile}', function(req, res){
+ res.type('text/plain');
+ res.send('_____\'s profile page');
+});
+
+app.get('/{movie-id}', function(req, res){
+ res.type('text/plain');
+ res.send('about this movie');
+});
+
+app.post('/index.html', function(req,res) {
+
+	connection.query({
+	  sql: 'SELECT * FROM `title` WHERE `title` = ?',
+	  timeout: 40000, // 40s
+	  values: ['movie_name']
+	}, function (err, results, fields) {
+
+		  if (err) throw err;
+
+		// error will be an Error if one occurred during the query
+  		// results will contain the results of the query
+  		// fields will contain information about the returned results fields (if any)
+	});
+});
+
+
+// custom 404 page
+app.use(function(req, res){
+ res.type('text/plain');
+ res.status(404);
+ res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+ console.error(err.stack);
+ res.type('text/plain');
+ res.status(500);
+ res.send('500 - Server Error');
+});
+
+
+// const PORT = 8080;
+// app.listen(PORT, function(){
+//     console.log('listening on port: ' + PORT);
+// });
+
+app.listen(app.get('port'), function(){
+    console.log( 'Express started up showchat' +
+                 app.get('port') + '; press Ctrl-C to terminate.');
 });
