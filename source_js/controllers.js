085a38371c48bf55681d8adab1e8708efc7bb0c0
Cuevas <icuevas2@fa16-cs411-36.cs.illinois.edu>
2016-11-20 16:41:47 -0600

    fine

--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,10 +1,47 @@
-var webControllers = angular.module('webControllers', []);
+var myApp = angular.module('myApp', []);
+var postApp = angular.module('postApp', []);
 
-webControllers.controller('SearchController', ['$scope', function($scope) {
-   $scope.data = "";
-   $scope.displayText = ""
+myApp.controller('movieSearchApp', ['$scope', '$http', '$window', function($scope, $http, $window) {
+    //console.log('myApp.controller hello');
+    $scope.search = function(){
+        console.log('in the search function');
+        $http.post('/movieFile', $scope.movie).success(function(results){
+            $scope.movielist = results;
+        });
+    }
 
-  $scope.setData = function(){
-    $scope.displayText = "Data set"
-  };
+    $scope.data = {model: null,
+                   availableOptions: [
+                     {id: '1', name: "episode"},
+                     {id: '2', name: "movie"},
+                     {id: '3', name: "tv mini series"},
+                     {id: '4', name: "tv movie"},
+                     {id: '5', name: "tv series"},
+                     {id: '6', name: "video game"},
+                     {id: '7', name: "video movie"}
+                  ]
+                };
 }]);
+
+postApp.controller('postCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
+	console.log("hello");
+	$http.get('/postlistData').success(function(results){
+		$scope.postlist=results;
+	});
+
+	$scope.createPost = function(){
+		$http.post('/postlistData', $scope.post);
+		$window.location.reload();
+	};
+}]);
+
+//movieSearchApp.controller('AppCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
+//	console.log("hello");
+//	$http.get('/movieList').success(function(results){
+//		$scope.contactlist=results;
+//	});
+//	// $scope.addContact = function(){
+//	// 	$http.post('/contactlist', $scope.contact);
+//    //     $window.location.reload();
+//	// };
+//}]);
--- source_js/script.js
+++ source_js/script.js
@@ -6,6 +6,42 @@ $(".button.alert").click(function(){
     {
         queryVal: searchBarVal
     });
+
+    // $.get(theUrl,
+    // {
+    //     urlVal: "search"
+    // });
+});
+
+$("#searchBar").keyup(function(event){
+    if(event.keyCode == 13){
+        $(".button.alert").click();
+    }
+});
+
+$("#Trending").click(function(){
+    var theUrl = "TrendingData";
 });
 
 
+$("#Trending").click(function(){
+    var theUrl = "TrendingData";
+});
+
+
+$("#Trending").click(function(){
+    var theUrl = "TrendingData";
+    $.post(theUrl,
+    {
+         queryVal: "Trending"
+    });
+});
+
+$("#Theater").click(function(){
+    var theUrl       = "TheaterData";
+    console.log("searchBarVal:", searchBarVal);
+    $.post(theUrl,
+    {
+        theaterVal: "Theater"
+    });
+});
